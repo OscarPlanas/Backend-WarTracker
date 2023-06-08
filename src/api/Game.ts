414d4ddbd83447ec91e19f1b63f48abf30c36cb3
OscarPlanas <casplanas7@gmail.com>
2023-06-08 20:35:33 +0200

    v0.9 Tournament table

--- src/api/Game.ts
+++ src/api/Game.ts
@@ -4,7 +4,6 @@ import { verifyToken, isOwner } from '../middlewares/authJWT'
 
 const router = Router();
 
-router.get('/', gameController.getall);
 router.get('/:id_game', gameController.getone);
 router.put('/:id', gameController.update);
 router.delete('/:id', gameController.deleteGame);
@@ -13,5 +12,6 @@ router.get('/tournament/:id_tournament', gameController.getByTournament);
 router.put('/tournament/:id_tournament/:id_player', gameController.addGamebyTournament);
 router.post('/tournament/:id_tournament', gameController.addGamebyTournamentID);
 router.delete('/tournament/:id_tournament', gameController.deleteGameByTournament);
-
+router.delete('/row/:_id', gameController.deleteRowByTournamentID);
+router.get('/', gameController.getall);
 export default router;
--- src/controller/gameController.ts
+++ src/controller/gameController.ts
@@ -1,17 +1,16 @@
 import { param } from 'express-validator';
 import Game from '../model/Game';
 import User from '../model/User';
+import Meeting from '../model/Meeting';
 
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
+import meetingController from './meetingController';
 const getall = async (req: Request, res: Response) => {
     const games = await Game.find().populate('tournament').populate('player');
     res.json(games);
 }
 const getone = async (req: Request, res: Response) => {
-    const game = await Game.findById(req.params.id_game).populate('meeting').populate({
-        path: 'comments',
-        populate: { path: 'competitor' }
-    });
+    const game = await Game.findById(req.params.id_game);
     if (!game) {
         return res.status(404).send('The game does not exist');
     }
@@ -24,12 +23,12 @@ const addGame = async (req: Request, res: Response) => {
     const user_reported = req.body.user_reported;
     const date = req.body.date;
     const newReport = new Report({
-		owner,
+        owner,
         user_reported,
         reason,
         date
         
-	});
+    });
     await newReport.save( (err: any) => {
         if (err) {
             return res.status(500).send(err);
@@ -37,49 +36,115 @@ const addGame = async (req: Request, res: Response) => {
         res.status(200).json({ status: 'Report saved' });
     });*/
     console.log("entro a addGame");
+    console.log("|");
+    /*  const meeting = await Meeting.findById(req.params.id).populate('organizer').populate('participants');
+    if (!meeting) {
+        return res.status(404).send('The meeting does not exist');
+    }
+    res*/
+    const meeting = await Meeting.findById(req.body[0].tournament).populate('organizer').populate('participants');
+    console.log(meeting);
+    console.log("PARTICIPANTS");
+    const participants = meeting!.participants;
+    const jsonParticipants = JSON.stringify(participants);
+    console.log(jsonParticipants);
+    const jsonParticipants2 = JSON.parse(jsonParticipants);
+    console.log(jsonParticipants2);
+    console.log(jsonParticipants2[0].username);
+    //console.log(jsonParticipants2.username);
+    console.log(jsonParticipants2[1].username);
+    console.log("separado");
+    console.log("^");
+    console.log(participants.length);
+
+    /* var json = req.body;
+     var keyCount = Object.keys(json).length;
+     console.log(keyCount);
+     for (var i = 0; i < keyCount; i++) {
+         console.log("entro al for");
+         console.log(participants[i]);
+         console.log(req.body[i].player);
+         var found = false;
+       
+         for (var j = 0; j < participants.length; j++) {
+           if (req.body[i].player === jsonParticipants2[j].username) {
+             console.log("entro al if");
+             console.log(participants[j]);
+             console.log(req.body[i].player);
+             found = true;
+             break; // Exit the inner loop once a match is found
+           }
+         }
+       
+         if (!found) {
+           console.log("This player is not in the tournament");
+           return res.status(500).json({ message: 'The player is not in the tournament' });
+         }
+       }*/
+
+
 
     console.log(req.body);
-    console.log(req.body['tournament']);
-    console.log(req.body[0].tournament);
-    console.log(req.body['player']);
-    console.log(req.body['alliance']);
-    console.log(req.body['difference_points']);
+    console.log(req.body['tournament'] + "50");
+    console.log(req.body[0].tournament + "51");
+    console.log(req.body['player'] + "52");
+    console.log(req.body['alliance'] + "53");
+    console.log(req.body['difference_points'] + "54");
     console.log(req.body.tournament);
     console.log(req.body.player);
     console.log(req.body.alliance);
     console.log(req.body.difference_points);
     console.log("decode");
-    
+
     var json = req.body;
-    var keyCount  = Object.keys(json).length;
+    var keyCount = Object.keys(json).length;
     console.log(keyCount);
+
+    var nonParticipantUsers = []; // Array to store non-participant usernames
+
     for (var i = 0; i < keyCount; i++) {
         if (!req.body[i]) {
             break;
         }
+        // Check if the player is a participant
+        var isParticipant = false;
+        for (var j = 0; j < participants.length; j++) {
+            if (req.body[i].player === jsonParticipants2[j].username) {
+                isParticipant = true;
+                break;
+            }
+        }
+
+        if (!isParticipant) {
+            console.log("Player is not a participant");
+            nonParticipantUsers.push(req.body[i].player); // Add the non-participant username to the array
+            continue; // Skip to the next iteration
+        }
+
+
         if (req.body[i].tournament == null || req.body[i].player == null) {
             return res.status(500).json({ message: 'Game not found' });
         }
-        if (req.body[i].alliance == null){
+        if (req.body[i].alliance == null) {
             req.body[i].alliance = "---";
         }
-        if (req.body[i].victory_points_against === ""){
+        if (req.body[i].victory_points_against === "") {
             console.log("entro a victory_points_against");
             req.body[i].victory_points_against = 0;
         }
-        if (req.body[i].victory_points_favour === ""){
+        if (req.body[i].victory_points_favour === "") {
             console.log("entro a victory_points_favour");
             req.body[i].victory_points_favour = 0;
         }
-        if (req.body[i].leaders_eliminated === ""){
+        if (req.body[i].leaders_eliminated === "") {
             console.log("entro a leaders_eliminated");
             req.body[i].leaders_eliminated = 0;
         }
-        if (req.body[i].games_played === ""){
+        if (req.body[i].games_played === "") {
             console.log("entro a games_played");
             req.body[i].games_played = 0;
         }
-        if (req.body[i].difference_points === ""){
+        if (req.body[i].difference_points === "") {
             console.log("entro a difference_points");
             req.body[i].difference_points = 0;
         }
@@ -108,16 +173,20 @@ const addGame = async (req: Request, res: Response) => {
         const leaders_eliminated = req.body[i].leaders_eliminated;
         const games_played = req.body[i].games_played;
         console.log(alliance);
+
         const newGame = new Game({ tournament, player, alliance, difference_points, victory_points_against, victory_points_favour, leaders_eliminated, games_played });
         console.log(newGame);
         await newGame.save();
         console.log("guardado");
-       
+
     }
-    
-    return res.status(200).json({ status: 'Game saved' });
-       
-      
+
+    if (nonParticipantUsers.length > 0) {
+        return res.status(200).json({ status: 'Games saved', nonParticipantUsers });
+    } else {
+        return res.status(200).json({ status: 'Games saved' });
+    }
+
 };
 
 const update = async (req: Request, res: Response) => {
@@ -196,7 +265,7 @@ const addGamebyTournament = async (req: Request, res: Response) => {
 
         //games.tournament = req.params.tournament;
 
-    
+
     }
 
 
@@ -225,7 +294,7 @@ const addGamebyTournamentID = async (req: Request, res: Response) => {
         res.status(200).json({ status: 'Game saved' });
     });
 }
-                
+
 
 /*const game = new Game(req.body);
     await game.save( (err: any) => {
@@ -247,6 +316,17 @@ const deleteGameByTournament = async (req: Request, res: Response) => {
     }
 }
 
+const deleteRowByTournamentID = async (req: Request, res: Response) => {
+    try {
+        await Game.deleteOne({ _id: req.params._id });
+        res.status(200).json({ status: 'Game deleted' });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Game not found', error });
+    }
+}
+
+
 export default {
     getall,
     getone,
@@ -256,5 +336,6 @@ export default {
     getByTournament,
     addGamebyTournament,
     addGamebyTournamentID,
-    deleteGameByTournament
+    deleteGameByTournament,
+    deleteRowByTournamentID
 }
